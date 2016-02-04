02d486dcba4f7815ddc9759ca3a8daec8be21dde
MantoshMishra57 <mantoshmishra@Mantoshs-MacBook-Pro.local>
2016-02-04 15:32:50 +0530

    Added main file

---
+++ ConsolDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ConsolDemo
+//
+//  Created by Mantosh Mishra on 03/02/16.
+//  Copyright © 2016 Mantosh Mishra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, Git!");
+    }
+    return 0;
+}
